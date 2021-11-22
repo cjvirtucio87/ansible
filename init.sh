952e919ad46fbbb112cd0d7188bb82ccb6c49847
CJ Virtucio <cjv287@gmail.com>
2021-11-22 07:53:52 -0500

    added STAGES and ANSIBLE_FLAGS vars to init.sh; refactored to group roles by single entrypoint role (cjvdev); added nordvpn, libreoffice and discord roles; added tags

--- init.sh
+++ init.sh
@@ -5,11 +5,28 @@ set -e
 ROOT_DIR="$(dirname "$(readlink --canonicalize "$0")")"
 readonly ROOT_DIR
 readonly ANSIBLE_VENV_DIR="${HOME}/.ansible-venv"
+readonly STAGES="${STAGES:-apt python play}"
 
-function main {
+function _apt {
   sudo apt-get update -y
   sudo apt-get install -y python3 python3-pip python3-venv lsb-core
+}
 
+function _play {
+  local play_args=(
+    --extra-vars "managed_user=$(whoami)"
+  )
+
+  if [[ -v ANSIBLE_FLAGS ]]; then
+    local flags
+    IFS=' ' read -ra flags <<<"${ANSIBLE_FLAGS}"
+    play_args+=("${flags[@]}")
+  fi
+
+  ansible-playbook ./init.yml "${play_args[@]}"
+}
+
+function _python {
   if [[ ! -d "${ANSIBLE_VENV_DIR}" ]]; then
     python3 -m venv "${ANSIBLE_VENV_DIR}"
     . "${ANSIBLE_VENV_DIR}/bin/activate"
@@ -18,10 +35,31 @@ function main {
     python -m pip install --requirement "${ROOT_DIR}/requirements.txt"
     deactivate
   fi
+}
 
+function main {
+  # shellcheck disable=SC1090,SC1091
   . "${ANSIBLE_VENV_DIR}/bin/activate"
 
-  ansible-playbook ./init.yml --extra-vars "managed_user=$(whoami)"
+  local stages
+  IFS=' ' read -ra stages <<<"${STAGES}"
+
+  for stage in "${stages[@]}"; do
+    case "${stage}" in
+      apt)
+        _apt
+        ;;
+      play)
+        _play
+        ;;
+      python)
+        _python
+        ;;
+      *)
+        >&2 echo "unsupported stage [${stage}]"
+        return 1
+    esac
+  done
 }
 
 main
